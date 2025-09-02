3c1bed5a79398dd4f47cc85171c036354901c270
KomalSinha-1718 <sinhakomal1417@gmail.com>
2025-09-03 02:21:23 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+import userRoutes from "./routes/userRoutes.js"
+import productRoutes from "./routes/productRoutes.js"
+import orderRoutes from "./routes/orderRoutes.js"
+
+dotenv.config()
+const app = express()
+
+// Middleware
+app.use(express.json())
+
+// Routes
+app.use("/api/users", userRoutes)
+app.use("/api/products", productRoutes)
+app.use("/api/orders", orderRoutes)
+
+// Connect DB & Start Server
+const PORT = process.env.PORT || 5000
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected")
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  })
+  .catch((err) => console.error(err))
